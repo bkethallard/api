a98e4c234b87f3770cbc488b5572d3ea15c85301
bkethallard <123136383+bkethallard@users.noreply.github.com>
2023-10-19 13:24:46 -0500

    ;jldasfjlk

--- routes/tasks.js
+++ routes/tasks.js
@@ -23,6 +23,19 @@ router.get('/tasks', async (req, res) => {
   }
 });
 
+// Get a task by ID
+router.get('/tasks/:id', async (req, res) => {
+  try {
+    const task = await Task.findById(req.params.id);
+    if (!task) {
+      return res.status(404).json({ error: 'Task not found' });
+    }
+    res.status(200).json(task);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
 // Update a task by ID
 router.patch('/tasks/:id', async (req, res) => {
   const updates = Object.keys(req.body);
@@ -37,7 +50,7 @@ router.patch('/tasks/:id', async (req, res) => {
     const task = await Task.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true });
 
     if (!task) {
-      return res.status(404).json();
+      return res.status(404).json({ error: 'Task not found' });
     }
 
     res.status(200).json(task);
@@ -52,7 +65,7 @@ router.delete('/tasks/:id', async (req, res) => {
     const task = await Task.findByIdAndDelete(req.params.id);
 
     if (!task) {
-      return res.status(404).json();
+      return res.status(404).json({ error: 'Task not found' });
     }
 
     res.status(200).json(task);
